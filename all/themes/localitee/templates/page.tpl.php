520f40b204fc8c7a9fb576fcbd180fda1f847a23
John Ossoway <john@ragingtrifle.com>
2012-07-16 13:07:19 +0100

    First commit of real theme.

--- all/themes/localitee/templates/page.tpl.php
+++ all/themes/localitee/templates/page.tpl.php
@@ -2,11 +2,10 @@
 
 /**
  * @file
- * Bartik's theme implementation to display a single Drupal page.
+ * Default theme implementation to display a single Drupal page.
  *
  * The doctype, html, head and body tags are not in this template. Instead they
- * can be found in the html.tpl.php template normally located in the
- * modules/system directory.
+ * can be found in the html.tpl.php template in this directory.
  *
  * Available variables:
  *
@@ -28,12 +27,6 @@
  *   in theme settings.
  * - $site_slogan: The slogan of the site, empty when display has been disabled
  *   in theme settings.
- * - $hide_site_name: TRUE if the site name has been toggled off on the theme
- *   settings page. If hidden, the "element-invisible" class is added to make
- *   the site name visually hidden, but still accessible.
- * - $hide_site_slogan: TRUE if the site slogan has been toggled off on the
- *   theme settings page. If hidden, the "element-invisible" class is added to
- *   make the site slogan visually hidden, but still accessible.
  *
  * Navigation:
  * - $main_menu (array): An array containing the Main menu links for the
@@ -63,185 +56,143 @@
  *   comment/reply/12345).
  *
  * Regions:
- * - $page['header']: Items for the header region.
- * - $page['featured']: Items for the featured region.
- * - $page['highlighted']: Items for the highlighted content region.
  * - $page['help']: Dynamic help text, mostly for admin pages.
+ * - $page['highlighted']: Items for the highlighted content region.
  * - $page['content']: The main content of the current page.
  * - $page['sidebar_first']: Items for the first sidebar.
- * - $page['triptych_first']: Items for the first triptych.
- * - $page['triptych_middle']: Items for the middle triptych.
- * - $page['triptych_last']: Items for the last triptych.
- * - $page['footer_firstcolumn']: Items for the first footer column.
- * - $page['footer_secondcolumn']: Items for the second footer column.
- * - $page['footer_thirdcolumn']: Items for the third footer column.
- * - $page['footer_fourthcolumn']: Items for the fourth footer column.
+ * - $page['sidebar_second']: Items for the second sidebar.
+ * - $page['header']: Items for the header region.
  * - $page['footer']: Items for the footer region.
  *
  * @see template_preprocess()
  * @see template_preprocess_page()
  * @see template_process()
- * @see bartik_process_page()
  * @see html.tpl.php
  */
 ?>
-<div id="page-wrapper"><div id="page">
 
-  <div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
+     <div class="navbar">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          
+          
+          <div class="btn-group pull-right account">
+            <a class="btn btn-success dropdown-toggle login_btn" data-toggle="dropdown" href="#" id="tab">
+              <i class="icon-cog"></i>  Sign in
+              <span class="caret"></span>
+            </a>
+                <div id="login_box">
+                    <div id="login_box_content">
+                    
+                         <?php print $login_form; ?>
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan">
-
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name">
-              <strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong>
-            </div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan">
-            <?php print $site_slogan; ?>
+                    </div>
+                </div>
+          </div> 
+          
+          <div id="branding">
+          	<div class="logo"><a href="<?php print $front_page; ?>">Localitee<span>.</span></a></div>
+          	<div class="postcode"><?php print $postcode; ?></div>
           </div>
-        <?php endif; ?>
-
-      </div> <!-- /#name-and-slogan -->
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-
-    <?php if ($main_menu): ?>
-      <div id="main-menu" class="navigation">
-        <?php print theme('links__system_main_menu', array(
-          'links' => $main_menu,
-          'attributes' => array(
-            'id' => 'main-menu-links',
-            'class' => array('links', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => t('Main menu'),
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </div> <!-- /#main-menu -->
-    <?php endif; ?>
-
-    <?php if ($secondary_menu): ?>
-      <div id="secondary-menu" class="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'id' => 'secondary-menu-links',
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => t('Secondary menu'),
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </div> <!-- /#secondary-menu -->
-    <?php endif; ?>
-
-  </div></div> <!-- /.section, /#header -->
-
-  <?php if ($messages): ?>
-    <div id="messages"><div class="section clearfix">
-      <?php print $messages; ?>
-    </div></div> <!-- /.section, /#messages -->
-  <?php endif; ?>
-
-  <?php if ($page['featured']): ?>
-    <div id="featured"><div class="section clearfix">
-      <?php print render($page['featured']); ?>
-    </div></div> <!-- /.section, /#featured -->
-  <?php endif; ?>
-
-  <div id="main-wrapper" class="clearfix"><div id="main" class="clearfix">
-
-    <?php if ($breadcrumb): ?>
-      <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-    <?php endif; ?>
-
-    <?php if ($page['sidebar_first']): ?>
-      <div id="sidebar-first" class="column sidebar"><div class="section">
-        <?php print render($page['sidebar_first']); ?>
-      </div></div> <!-- /.section, /#sidebar-first -->
-    <?php endif; ?>
-
-    <div id="content" class="column"><div class="section">
-      <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title">
-          <?php print $title; ?>
-        </h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php if ($tabs): ?>
-        <div class="tabs">
-          <?php print render($tabs); ?>
+          
+          <div class="nav-collapse">
+          
+           <?php if ($main_menu): ?>
+           <?php print theme('links__system_main_menu', array(
+	           'links' => $main_menu,
+	           'attributes' => array(
+	          	 'class' => array('nav'),
+	          	 'role' => 'navigation',
+	           ),
+          	)); ?>
+        	<?php endif; ?>
+          
+          </div><!--/.nav-collapse -->
         </div>
-      <?php endif; ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links">
-          <?php print render($action_links); ?>
-        </ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
+      </div>
+      
+          <?php print $messages; ?>
 
-    </div></div> <!-- /.section, /#content -->
+      
+    </div>
+    
+   <div class="container-fluid">
+   
+      <div class="row-fluid advancedsearch">
+        <div class="span12">
+        
+          <div class="searchbox" role="search">
+            
+          	 <?php print $search; ?>
+            
+          </div><!-- /.searchbox -->
+        </div><!-- /.span12 -->
+      </div><!-- /.row-fluid -->
+      
+      <div class="row-fluid" id="maincontent" role="main">
+          
+            <div class="span8">
+            
+            
+               <?php if (drupal_is_front_page()): ?>
+            
+               		<?php if ($page['content_top']): ?>
+            
+               		<div class="col2box">
+            	
+	               		<?php print render($page['content_top']); ?>         	
+            	
+	               	</div><!-- /.col2box -->
+	               	
+	               	<?php endif; ?>
+	               	
+	               	<?php print render($page['content']); ?>	
 
-    <?php if ($page['sidebar_second']): ?>
-      <div id="sidebar-second" class="column sidebar"><div class="section">
-        <?php print render($page['sidebar_second']); ?>
-      </div></div> <!-- /.section, /#sidebar-second -->
-    <?php endif; ?>
+              <?php else: ?>          
+              
+              		<div class="box">
+              			<?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+              			<?php if ($page['content_top']): ?>
+                        	
+	               			<?php print render($page['content_top']); ?>         	
+            		               	
+	               		<?php endif; ?>
+              			<?php print render($page['content']); ?>
+              		</div>
+              
+              <?php endif; ?>
+                          
+            </div><!-- /.span8 -->
+            
+            <div class="span4">
+	            
+	          <?php if ($page['sidebar_first']): ?>
+	          	<?php print render($page['sidebar_first']); ?>
+	          <?php endif; ?>	            
+	            
+            </div><!-- /.span4 -->            
+            
+      </div><!-- /.row-fluid -->
 
-  </div></div> <!-- /#main, /#main-wrapper -->
+      <footer>
+      
+      	 <div class="row-fluid">
+      	 
+      	 	 <div class="span12">
+	      	 
+	      	 	      
+	      	 	<?php print render($page['footer']); ?>
+	      	 
+	      	 
+	      	 </div><!-- /.span12 -->
+      	      	 
+      	 </div>
+        
+      </footer>
 
-  <?php if ($page['triptych_first'] || $page['triptych_middle'] || $page['triptych_last']): ?>
-    <div id="triptych-wrapper"><div id="triptych" class="clearfix">
-      <?php print render($page['triptych_first']); ?>
-      <?php print render($page['triptych_middle']); ?>
-      <?php print render($page['triptych_last']); ?>
-    </div></div> <!-- /#triptych, /#triptych-wrapper -->
-  <?php endif; ?>
-
-  <div id="footer-wrapper"><div class="section">
-
-    <?php if ($page['footer_firstcolumn'] || $page['footer_secondcolumn'] || $page['footer_thirdcolumn'] || $page['footer_fourthcolumn']): ?>
-      <div id="footer-columns" class="clearfix">
-        <?php print render($page['footer_firstcolumn']); ?>
-        <?php print render($page['footer_secondcolumn']); ?>
-        <?php print render($page['footer_thirdcolumn']); ?>
-        <?php print render($page['footer_fourthcolumn']); ?>
-      </div> <!-- /#footer-columns -->
-    <?php endif; ?>
-
-    <?php if ($page['footer']): ?>
-      <div id="footer" class="clearfix">
-        <?php print render($page['footer']); ?>
-      </div> <!-- /#footer -->
-    <?php endif; ?>
-
-  </div></div> <!-- /.section, /#footer-wrapper -->
-
-</div></div> <!-- /#page, /#page-wrapper -->
+    </div><!--/.fluid-container-->
