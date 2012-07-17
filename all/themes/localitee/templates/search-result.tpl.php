a4eb06c9b2c0d65b3da83628f472312c34d3323d
John Ossoway <john@ragingtrifle.com>
2012-07-17 13:42:23 +0100

    Edited search result template to format results.

--- all/themes/localitee/templates/search-result.tpl.php
+++ all/themes/localitee/templates/search-result.tpl.php
@@ -63,17 +63,14 @@
  */
 ?>
 <li class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php print render($title_prefix); ?>
-  <h3 class="title"<?php print $title_attributes; ?>>
+  <h4 class="title"<?php print $title_attributes; ?>>
     <a href="<?php print $url; ?>"><?php print $title; ?></a>
-  </h3>
-  <?php print render($title_suffix); ?>
-  <div class="search-snippet-info">
+  </h4>
     <?php if ($snippet): ?>
       <p class="search-snippet"<?php print $content_attributes; ?>><?php print $snippet; ?></p>
     <?php endif; ?>
     <?php if ($info): ?>
       <p class="search-info"><?php print $info; ?></p>
     <?php endif; ?>
-  </div>
+    	<p><a href="<?php print $url; ?>" class="btn btn-success btn-mini">View details</a></p>
 </li>
